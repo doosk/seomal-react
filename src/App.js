209d0a7b75c94be000d1cd5de6086e3d95c830e5
doosk <adm3@naver.com>
2024-11-04 20:51:19 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
+function Header({ title, onHeaderClick }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <header>
+      <h1>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='/'
+          onClick={(e) => {
+            e.preventDefault();
+            onHeaderClick();
+          }}
         >
-          Learn React
+          {title}
         </a>
-      </header>
+      </h1>
+    </header>
+  );
+}
+function Nav(props) {
+  return (
+    <nav>
+      <ol>
+        {props.topics.map((item) => (
+          <li key={item.id}>
+            <a
+              id={item.id}
+              href={`'/read/${item.id}'`}
+              onClick={(e) => {
+                e.preventDefault();
+                props.onNavClick(e.target.id);
+              }}
+            >
+              {item.title}
+            </a>
+          </li>
+        ))}
+      </ol>
+    </nav>
+  );
+}
+// Nav.propTypes = {
+//   id: PropTypes.number.isRequired,
+//   title: PropTypes.string.isRequired,
+//   body: PropTypes.string.isRequired,
+// };
+function Article({ title, body }) {
+  return (
+    <article>
+      <h2>{title}</h2>
+      {body}
+    </article>
+  );
+}
+function App() {
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+
+  const topics = [
+    { id: 1, title: 'html', body: 'html is ...' },
+    { id: 2, title: 'css', body: 'css is ...' },
+    { id: 3, title: 'javascript', body: 'javascript is ...' },
+  ];
+  const onClickHeader = () => setMode('WELCOME');
+
+  let content = null;
+  if (mode === 'WELCOME') {
+    content = <Article title='Welcome' body='Hello, WEB' />;
+  } else if (mode === 'READ') {
+    const { title, body } = topics.find((item) => item.id === id);
+    content = <Article title={title} body={body} />;
+  }
+  return (
+    <div>
+      <Header title='REACT' onHeaderClick={onClickHeader} />
+      <Nav
+        topics={topics}
+        onNavClick={(_id) => {
+          setMode('READ');
+          setId(Number(_id));
+        }}
+      />
+      {content}
     </div>
   );
 }
